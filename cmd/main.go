7a68c9f1a537710ffc7aba9e8ea229ac15f95eb6
arthurdotwork <35616365+arthurdotwork@users.noreply.github.com>
2025-04-05 15:08:55 +0200

    feat: implement basic gateway

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os/signal"
+	"syscall"
+
+	"github.com/arthurdotwork/heimdall"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGKILL)
+	defer stop()
+
+	gateway, err := heimdall.New("./config.yaml")
+	if err != nil {
+		slog.ErrorContext(ctx, "failed to initialize gateway", "error", err)
+		return
+	}
+
+	slog.InfoContext(ctx, "Starting gateway...")
+	if err := gateway.Start(ctx); err != nil {
+		slog.ErrorContext(ctx, "failed to start gateway", "error", err)
+		return
+	}
+}
